be6822b360dbc8a5e064f6e708553d9301621d44
David Rauschenbach <davidr@imap.cc>
2017-10-28 19:01:59 -0700

    Moses author provides a simplified unzip impl

--- src/stuart-ml/util/unzip.lua
+++ src/stuart-ml/util/unzip.lua
@@ -1,22 +1,9 @@
 local moses = require 'moses'
 
+local unpack = table.unpack or unpack
+
 local unzip = function(array)
-  local length = 0
-  array = moses.filter(array, function(_, v)
-    if moses.isTable(v) then
-      length = math.max(length, #v)
-      return true
-    end
-  end)
-  local result = {}
-  for i=1,length do
-    local t = {}
-    for j=1,#array do
-      t[#t+1] = array[j][i]
-    end
-    result[#result+1] = t
-  end
-  return result
+  return moses.zip(unpack(array))
 end
 
 return unzip
