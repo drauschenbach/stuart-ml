3cf2df85d8013c08258baf78c8959dca8f084f0f
David Rauschenbach <davidr@imap.cc>
2017-11-05 13:27:33 -0800

    Port KMeans fastSquaredDistance(), findClosest(), and pointCost()

--- src/stuart-ml/clustering/KMeans.lua
+++ src/stuart-ml/clustering/KMeans.lua
@@ -1,4 +1,5 @@
 local class = require 'middleclass'
+local MLUtils = require 'stuart-ml.util.MLUtils'
 
 local RANDOM = "random"
 local K_MEANS_PARALLEL = "k-means||"
@@ -14,6 +15,29 @@ function KMeans:initialize(k, maxIterations, initializationMode, initializationS
   self.initializationSteps = initializationSteps or 2
   self.epsilon = epsilon or 1e-4
   self.seed = seed or math.random(32000)
+end
+
+-- Returns the squared Euclidean distance between two vectors
+function KMeans.fastSquaredDistance(vectorWithNorm1, vectorWithNorm2)
+  return MLUtils.fastSquaredDistance(vectorWithNorm1.vector, vectorWithNorm1.norm, vectorWithNorm2.vector, vectorWithNorm2.norm)
+end
+
+-- returns a Lua 1-based index
+function KMeans.findClosest(centers, point)
+  local bestDistance = math.huge
+  local bestIndex = 1
+  for i,center in ipairs(centers) do
+    local lowerBoundOfSqDist = center.norm - point.norm
+    lowerBoundOfSqDist = lowerBoundOfSqDist * lowerBoundOfSqDist
+    if lowerBoundOfSqDist < bestDistance then
+      local distance = KMeans.fastSquaredDistance(center, point)
+      if distance < bestDistance then
+        bestDistance = distance
+        bestIndex = i
+      end
+    end
+  end
+  return bestIndex, bestDistance
 end
 
 function KMeans:getInitializationMode()
@@ -34,6 +58,19 @@ end
 
 function KMeans:getSeed()
   return self.seed
+end
+
+function KMeans:initKMeansParallel()
+  error('not implemented yet')
+end
+
+function KMeans:initRandom(vectorsWithNormsRDD)
+  return vectorsWithNormsRDD:takeSample(false, self.k, os.clock()*1e6)
+end
+
+function KMeans.pointCost(centers, point)
+  local _, bestDistance = KMeans.findClosest(centers, point)
+  return bestDistance
 end
 
 function KMeans:setInitialModel(model)
