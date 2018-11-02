78a93fe7c634bfe3ac43c106fe401a39762d7de8
David Rauschenbach <davidr@imap.cc>
2018-11-02 10:51:57 -0700

    Remove module upvalues

--- src/stuart-ml/linalg/BLAS.lua
+++ src/stuart-ml/linalg/BLAS.lua
@@ -1,8 +1,3 @@
-local DenseVector = require 'stuart-ml.linalg.DenseVector'
-local isInstanceOf = require 'stuart.util'.isInstanceOf
-local SparseVector = require 'stuart-ml.linalg.SparseVector'
-local Vector = require 'stuart-ml.linalg.Vector'
-
 local M = {}
 
 --[[ y += a * x
@@ -11,10 +6,14 @@ local M = {}
 @param vectorY Vector
 --]]
 M.axpy = function(a, vectorX, vectorY)
+  local isInstanceOf = require 'stuart.util'.isInstanceOf
+  local Vector = require 'stuart-ml.linalg.Vector'
   assert(isInstanceOf(vectorX, Vector))
   assert(isInstanceOf(vectorY, Vector))
   assert(vectorX:size() == vectorY:size())
+  local DenseVector = require 'stuart-ml.linalg.DenseVector'
   if vectorY:isInstanceOf(DenseVector) then
+    local SparseVector = require 'stuart-ml.linalg.SparseVector'
     if vectorX:isInstanceOf(SparseVector) then
       return M.axpy_sparse_dense(a,vectorX,vectorY)
     elseif vectorX:isInstanceOf(DenseVector) then
@@ -41,9 +40,13 @@ end
 
 M.dot = function(x, y)
   assert(x:size() == y:size())
+  local isInstanceOf = require 'stuart.util'.isInstanceOf
+  local DenseVector = require 'stuart-ml.linalg.DenseVector'
   if x:isInstanceOf(DenseVector) and y:isInstanceOf(DenseVector) then
     return M.dot_sparse_dense(x:toSparse(), y)
-  elseif x:isInstanceOf(SparseVector) and y:isInstanceOf(DenseVector) then
+  end
+  local SparseVector = require 'stuart-ml.linalg.SparseVector'
+  if x:isInstanceOf(SparseVector) and y:isInstanceOf(DenseVector) then
       return M.dot_sparse_dense(x, y)
   elseif x:isInstanceOf(DenseVector) and y:isInstanceOf(SparseVector) then
       return M.dot_sparse_dense(y, x)
